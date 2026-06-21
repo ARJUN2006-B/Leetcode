775c3c900d0308c0480977b2517df5302665f8a1
ARJUN2006-B <arjunmagesh28@gmail.com>
2026-06-21 12:58:58 +0530

    Added solution for problem 485

---
+++ 485. Max Consecutive Ones.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int j=0;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                j++;
+            }
+            else{
+                if(j>max){
+                    max=j;
+                }
+                j=0;
+            }
+        }
+        if(j>max)
+        {
+             max=j;
+        }
+        return max;
+    }
+}
